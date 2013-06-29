8051100195841f45d8f36b053fa804b3f4020ca9
Todd Hainsworth <hainsworth.todd@gmail.com>
2013-06-29 14:31:38 +0930

    this works better

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "------ Installing dependancies ------"
-apt-get install xclip
+sudo apt-get install xclip
 gem install clipboard
 echo "------ Installing clp to /usr/bin/ ------"
 cp clp /usr/bin/clp
